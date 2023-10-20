6d187d9f03c5117d64c2305970b12e661b0d1a6f
Иван Шарый <lliy228@yandex.ru>
2023-10-20 22:22:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sities = { "Moscow,ru", "Orel,ru", "Volgograd,ru","London,uk" };
+            Console.WriteLine("Ввыберите город из списка");
+            for (int i = 0; i < Sities.Length; i++)
+            {
+                Console.WriteLine($"{i}.){Sities[i]}");
+            }
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                HttpClient client = new HttpClient() { BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/weather") };
+                var ans = client.GetAsync($"?q={Sities[a]}&APPID=0d3690ee5490e8e63b5d97cd3b63a257");
+                var json = ans.Result.Content.ReadAsStringAsync().Result;
+                var ansver = JsonConvert.DeserializeObject<Root>(json.ToString());
+
+                Console.WriteLine($"В городе {ansver.name}\r\nTemp:{ansver.main.temp};TempMax:{ansver.main.temp_max};TempMin:{ansver.main.temp_min}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Для выбора города необходимо указать его число!\r\nПовторите попытку еще раз!");
+            }
+        }
+    }
+}
